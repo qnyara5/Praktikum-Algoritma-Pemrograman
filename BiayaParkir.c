4c6c0cc8321b001dba325546ee539fc4b7ec159d
qnyara5 <100355057+qnyara5@users.noreply.github.com>
2022-03-12 19:54:38 +0700

    PraktikumAlpro3/

---
+++ BiayaParkir.c
@@ -0,0 +1,34 @@
+/* Nama file    : BiayaParkir.c*/
+/* Deskripsi    : Menentukan besarnya biaya parkir yang dihitung
+                berdasarkan lamanya parkir dan menampilkannya di layar*/
+/* Nama         : Tiara Fitra Ramadhani Siregar*/
+/* NIM          : 24060121120008*/
+
+#include <stdio.h>
+
+int main(){
+    /*Kamus*/
+    int jam;
+    int biaya;
+
+    /* Algoritma */
+    printf("=========== Menghitung Biaya Parkir ===========\n");
+    printf("===============================================\n");
+    printf("\nMasukan waktu parkir : ");
+
+        /*Input*/
+    scanf("\n %d", &jam);
+
+        /*Proses */
+    if (0 < jam && jam <= 2){
+        biaya = 2000;
+    } else {
+        biaya = 2000 + (jam - 2) * 500;
+    }
+        /* Output */
+    printf("\n===============================================\n");
+    printf("\nLamanya parkir %d jam, biaya parkirnya Rp. %d\n", jam, biaya);
+    printf("\n===============================================\n");
+    return 0;
+}
+
---
+++ UpahKerja.c
@@ -0,0 +1,56 @@
+/* Nama file    : UpahKerja.c*/
+/* Deskripsi    : Menghitung besarnya upah kerja yang diterima per
+                minggu dan menampilkannya di layar*/
+/* Nama         : Tiara Fitra Ramadhani Siregar*/
+/* NIM          : 24060121120008*/
+
+#include <stdio.h>
+
+int main(){
+    /* Kamus */
+    int gol;
+    int upah;
+    int jam;
+    int utotal;
+
+    /* Algoritma */
+    printf("=========== Menghitung Upah Kerja =============\n");
+    printf("===============================================\n");
+
+        /* Input */
+    printf("\nMasukan golongan pekerja (pilih 1/ 2/ 3/ 4) : ");
+    scanf("\n %d", &gol);
+    printf("\nMasukan jam kerja per minggu : ");
+    scanf("\n %d", &jam);
+
+        /* Proses */
+    switch (gol){
+        case 1 : upah = 1000;
+        break;
+        case 2 : upah = 1500;
+        break;
+        case 3 : upah = 2000;
+        break;
+        case 4 : upah = 2500;
+        break;
+        default:
+            printf("\n===============================================\n");
+            printf("\nGolongan tidak tersedia\n");
+            printf("\n===============================================\n");
+        break;
+        }
+
+    if (0 < jam && jam < 40){
+        utotal = jam * upah;
+    }else {
+        utotal = upah * 40 + ((jam - 40) * 1.5 * upah);
+        }
+
+        /*output*/
+    printf("\n===============================================\n");
+    printf("\nUpah kerja per minggu Rp. %d \n", utotal);
+    printf("\n===============================================\n");
+
+    return 0;
+}
+
